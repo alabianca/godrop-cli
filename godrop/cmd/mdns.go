c9d1796039b8954c3e3dbf8808fe55c9aa1f497c
Alexander <labianca.alex@gmail.com>
2019-02-07 21:12:31 -0600

    remove need for public/private keys

--- godrop/cmd/mdns.go
+++ godrop/cmd/mdns.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 
@@ -22,18 +21,6 @@ func configGodropMdns() (*godrop.Godrop, error) {
 }
 
 func mdnsConfig(drop *godrop.Godrop) {
-
-	pubKey, puberr := loadPublicKey()
-	prvKey, prverr := loadPrivateKey()
-
-	if puberr != nil || prverr != nil {
-		drop.PrvKey = nil
-		drop.PubKey = nil
-		log.Println("Warning: Could not load private/public key pair. Communication will not be encrypted")
-	} else {
-		drop.PrvKey = prvKey
-		drop.PubKey = pubKey
-	}
 
 	drop.Port = viper.GetInt("LocalPort")
 	drop.Host = viper.GetString("Host")
