ffa876e5cef4fbb2cafc9ca55e1441c39d097383
Alexander <labianca.alex@gmail.com>
2019-02-10 19:04:43 -0600

    move advertise logic into godrop server library

--- godrop/cmd/advertise.go
+++ godrop/cmd/advertise.go
@@ -1,15 +1,10 @@
 package cmd
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"sync"
 	"syscall"
-
-	"github.com/alabianca/godrop"
 
 	"github.com/spf13/cobra"
 )
@@ -33,15 +28,21 @@ func runAdvertise(command *cobra.Command, args []string) {
 
 	fPath := args[0]
 
+	if err := checkFile(fPath); err != nil {
+		log.Fatal(err)
+	}
+
 	drop, err := configGodropMdns()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	advertiser, err := NewAdvertiser(fPath, drop)
+	server, err := drop.NewMDNSService(fPath)
 
-	defer advertiser.Shutdown()
+	go server.Start()
+
+	defer server.Shutdown()
 
 	if err != nil {
 		log.Fatal(err)
@@ -62,71 +63,6 @@ func init() {
 
 }
 
-type Advertiser struct {
-	sharePath string
-	server    *godrop.Server
-	shutDown  chan int
-	wg        *sync.WaitGroup
-}
-
-func (ad *Advertiser) Shutdown() {
-	log.Println("shutting down advertiser...")
-	ad.server.Shutdown()
-	ad.shutDown <- 1
-	ad.wg.Wait()
-}
-
-func NewAdvertiser(sharePath string, drop *godrop.Godrop) (*Advertiser, error) {
-	if err := checkFile(sharePath); err != nil {
-		return nil, err
-	}
-
-	server, err := drop.NewMDNSService(sharePath)
-
-	if err != nil {
-		return nil, err
-	}
-
-	ad := &Advertiser{
-		sharePath: sharePath,
-		server:    server,
-		shutDown:  make(chan int),
-		wg:        new(sync.WaitGroup),
-	}
-
-	go acceptLoop(ad.sharePath, ad.wg, ad.server, ad.shutDown)
-
-	return ad, nil
-}
-
-func acceptLoop(dir string, wg *sync.WaitGroup, server *godrop.Server, shutdown chan int) {
-	wg.Add(1)
-	sessionChan := make(chan *godrop.Session)
-	go func(s chan *godrop.Session) {
-		for {
-			sesh, err := server.Accept()
-
-			if err != nil {
-				log.Println("could not accept connection ", err)
-				continue
-			}
-
-			s <- sesh
-		}
-	}(sessionChan)
-
-	for {
-		select {
-		case <-shutdown:
-			log.Println("Shutting down accept loop")
-			wg.Done()
-			return
-		case sesh := <-sessionChan:
-			go transferDir(dir, sesh)
-		}
-	}
-}
-
 func checkFile(fPath string) error {
 	_, err := os.Stat(fPath)
 
@@ -135,60 +71,4 @@ func checkFile(fPath string) error {
 	}
 
 	return nil
-}
-
-func transferDir(dir string, s *godrop.Session) error {
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		writeHeader(s, path, info)
-
-		if info.IsDir() {
-			return nil
-		}
-
-		file, err := os.Open(path)
-		defer file.Close()
-
-		if err != nil {
-			return err
-		}
-
-		//write file
-		buf := make([]byte, BUF_SIZE)
-
-		for {
-			n, err := file.Read(buf)
-
-			if err != nil {
-				if err == io.EOF {
-					log.Println("Transferred: ", info.Name())
-					break
-				} else {
-					log.Println("Error reading: ", info.Name())
-				}
-			}
-
-			s.Write(buf[:n])
-			s.Flush()
-		}
-
-		return nil
-
-	})
-
-	return err
-}
-
-func writeHeader(s *godrop.Session, path string, info os.FileInfo) {
-	header := godrop.Header{
-		Name:  info.Name(),
-		Size:  info.Size(),
-		IsDir: info.IsDir(),
-		Path:  path,
-	}
-
-	s.WriteHeader(header)
 }
